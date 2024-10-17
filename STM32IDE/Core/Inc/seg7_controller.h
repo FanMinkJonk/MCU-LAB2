18bdba690021e8a28becab284d08be35e4d97277
FanMinkJonk <127284415+FanMinkJonk@users.noreply.github.com>
2024-10-17 20:21:50 +0700

    Finish and Run Ex1

---
+++ STM32IDE/Core/Inc/seg7_controller.h
@@ -0,0 +1,19 @@
+/*
+ * seg7_controller.h
+ *
+ *  Created on: Oct 17, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_SEG7_CONTROLLER_H_
+#define INC_SEG7_CONTROLLER_H_
+
+//--- Private Include ----------------------------
+
+#include "main.h"
+
+//--- Functions Declaration ----------------------
+
+void display7SEG(int num);
+
+#endif /* INC_SEG7_CONTROLLER_H_ */
